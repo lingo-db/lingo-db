80ee4576ae54dd45f61fedb98754c843ec0a095b
Michael Jungmair <jungmair@in.tum.de>
2021-10-08 09:07:46 +0200

    update example.py

--- tools/pymlirdb/example.py
+++ tools/pymlirdb/example.py
@@ -8,15 +8,6 @@ nation=pa.ipc.open_file(pa.OSFile('../../resources/data/tpch-1/nation.arrow')).r
 
 pymlirdb.load_tables({"supplier":supplier,"lineitem":lineitem,"orders":orders,"nation":nation})
 print("done loading!")
-def count_delayed_orders_for_supplier(suppkey):
-    items = pymlirdb.read_table("lineitem")
-    items = items[items["l_suppkey"]==suppkey]
-    orders = pymlirdb.read_table("orders")
-    orders = orders[orders["o_orderstatus"] == 'F']
-    orders = orders.join(items, on = (orders["o_orderkey"] == items["l_orderkey"]), how="left")
-    orders["delayed"] = is_delayed(orders["o_orderkey"], suppkey)
-    orders = orders[orders["delayed"] == True]
-    return orders.count()
 
 def is_delayed(orderkey, suppkey):
     containsDelayed = False
@@ -34,8 +25,18 @@ def is_delayed(orderkey, suppkey):
                 onlyDelayed = False
     return containsDelayed and containsOther and onlyDelayed
 
-pymlirdb.registerFunction(Function("count_delayed_orders_for_supplier",[DBType("int",["64"])],DBType("int",["64"]),count_delayed_orders_for_supplier))
+def count_delayed_orders_for_supplier(suppkey):
+    items = pymlirdb.read_table("lineitem")
+    items = items[items["l_suppkey"]==suppkey]
+    orders = pymlirdb.read_table("orders")
+    orders = orders[orders["o_orderstatus"] == 'F']
+    orders = orders.join(items, on = (orders["o_orderkey"] == items["l_orderkey"]), how="left")
+    orders["delayed"] = is_delayed(orders["o_orderkey"], suppkey)
+    orders = orders[orders["delayed"] == True]
+    return orders.count()
+
 pymlirdb.registerFunction(Function("is_delayed",[DBType("int",["64"]),DBType("int",["64"])],DBType("bool"),is_delayed))
+pymlirdb.registerFunction(Function("count_delayed_orders_for_supplier",[DBType("int",["64"])],DBType("int",["64"]),count_delayed_orders_for_supplier))
 
 df = pymlirdb.query("""
                         select s_name, count_delayed_orders_for_supplier(s_suppkey) as numwait
