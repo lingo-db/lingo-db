2608422b4f2c882d0c42970a84e56f66ecd5b356
Michael Jungmair <jungmair@in.tum.de>
2022-04-19 13:20:07 +0200

    Tools: fix filename + linter

--- tools/mlir-tools/mlir-to-sql.cpp
+++ tools/mlir-tools/mlir-to-sql.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Support/ErrorOr.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/SourceMgr.h"
+
 #include "mlir/Dialect/DB/IR/DBDialect.h"
 #include "mlir/Dialect/DB/IR/DBOps.h"
 #include "mlir/Dialect/DSA/IR/DSADialect.h"
@@ -15,7 +16,6 @@
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Parser/Parser.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #include <iostream>
 #include <list>
