df84b9d3fa919d3f5c6daf13351a851b84d1da8b
Alexis Engelke <engelke@in.tum.de>
2022-04-21 10:49:17 +0200

    DB: Unify logical operations to common base class
    All logical operations return a bool, which is nullable if one of the
    input operands is nullable. This removes some duplicated code.

--- lib/DB/DBOps.cpp
+++ lib/DB/DBOps.cpp
@@ -83,20 +83,6 @@ LogicalResult inferDivReturnType(MLIRContext* context, Optional<Location> locati
    }
    return success();
 }
-LogicalResult mlir::db::CmpOp::inferReturnTypes(
-   MLIRContext* context, Optional<Location> location, ValueRange operands,
-   DictionaryAttr attributes, RegionRange regions,
-   SmallVectorImpl<Type>& inferredReturnTypes) {
-   inferredReturnTypes.assign({constructNullableBool(context, operands)});
-   return success();
-}
-LogicalResult mlir::db::BetweenOp::inferReturnTypes(
-   MLIRContext* context, Optional<Location> location, ValueRange operands,
-   DictionaryAttr attributes, RegionRange regions,
-   SmallVectorImpl<Type>& inferredReturnTypes) {
-   inferredReturnTypes.assign({constructNullableBool(context, operands)});
-   return success();
-}
 
 ::mlir::LogicalResult verify(mlir::db::RuntimeCall runtimeCall) {
    auto reg = runtimeCall.getContext()->getLoadedDialect<mlir::db::DBDialect>()->getRuntimeFunctionRegistry();
