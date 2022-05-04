3212d7447e6a41d9230900617406c82548109963
Michael Jungmair <jungmair@in.tum.de>
2022-05-04 09:47:44 +0200

    Docker: fix reproduce

--- tools/scripts/reproduce-commit.sh
+++ tools/scripts/reproduce-commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $1
 DOCKER_BUILDKIT=1 docker build -f "docker/Dockerfile" -t mlirdb-repr:latest --build-arg builtImage=ghcr.io/jungmair/mlirdb:$1 --target reproduce "."
-docker run --privileged -it mlirdb-repr /bin/bash -c "python3 tools/benchmark-tpch.py /build/mlirdb/ tpch-1"
+docker run --privileged -it mlirdb-repr /bin/bash -c "python3 tools/scripts/benchmark-tpch.py /build/mlirdb/ tpch-1"
