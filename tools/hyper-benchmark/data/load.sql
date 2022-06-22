0e87da2c714a11e5dcb21c266d54cfef0b337436
Michael Jungmair <jungmair@in.tum.de>
2022-06-22 11:17:46 +0200

    Repo: remove unrelated scripts

--- tools/hyper-benchmark/data/load.sql
+++
@@ -1,8 +0,0 @@
-copy part from 'part.tbl' delimiter '|';
-copy supplier from 'supplier.tbl' delimiter '|';
-copy partsupp from 'partsupp.tbl' delimiter '|';
-copy customer from 'customer.tbl' delimiter '|';
-copy orders from 'orders.tbl' delimiter '|';
-copy lineitem from 'lineitem.tbl' delimiter '|';
-copy nation from 'nation.tbl' delimiter '|';
-copy region from 'region.tbl' delimiter '|';
