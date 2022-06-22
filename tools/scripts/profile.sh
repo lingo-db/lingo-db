3129788ae32ab7e094b24f29331dda5e196d884c
Michael Jungmair <jungmair@in.tum.de>
2022-06-22 14:50:31 +0200

    Repo: improve structure of tools directory

--- tools/scripts/profile.sh
+++ tools/scripts/profile.sh
@@ -2,5 +2,5 @@ rm events.parquet
 rm operations.parquet
 ./build/lingodb-debug/tools/sourcemap/sourcemap snapshot-0.mlir snapshot-1.mlir snapshot-2.mlir snapshot-3.mlir > sourcemap.json
 perf script  -s tools/scripts/extract-perf-data.py -i perf.data
-./tools/venv/bin/python3 tools/profile-viz/create_db.py
-./tools/venv/bin/python3 tools/profile-viz/run.py
+python3 tools/profile-tools/create_db.py
+python3 tools/profile-tools/run.py
