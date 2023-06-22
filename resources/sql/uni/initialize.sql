02994593c53a28f654ec6912e5e419d7a7f0bb4e
Michael Jungmair <jungmair@in.tum.de>
2023-06-22 11:48:15 +0200

    Repo: fix generation script for resources/data/uni

--- resources/sql/uni/initialize.sql
+++ resources/sql/uni/initialize.sql
@@ -5,7 +5,7 @@ create table vorlesungen (vorlnr bigint, titel varchar(30) not null, sws bigint 
 create table voraussetzen (vorgaenger bigint not null, nachfolger bigint not null, primary key (vorgaenger, nachfolger));
 create table hoeren (matrnr bigint not null, vorlnr bigint not null, primary key (matrnr, vorlnr));
 create table assistenten (persnr bigint, name varchar(30) not null, fachgebiet varchar(30) not null, boss bigint not null, primary key (persnr));
-create table pruefen (matrnr bigint not null, vorlnr bigint not null, persnr bigint not null, note decimal(2,1) not null, primary key (matrnr, vorlnr, persnr);
+create table pruefen (matrnr bigint not null, vorlnr bigint not null, persnr bigint not null, note decimal(2,1) not null, primary key (matrnr, vorlnr, persnr));
 insert into professoren(persnr, name, rang, raum) values (2125,'Sokrates','C4',226), (2126,'Russel','C4',232), (2127,'Kopernikus','C3',310), (2133,'Popper','C3',52), (2134,'Augustinus','C3',309), (2136,'Curie','C4',36), (2137,'Kant','C4',7);
 insert into studenten(matrnr, name, semester) values (24002,'Xenokrates',18), (25403,'Jonas',12), (26120,'Fichte',10), (26830,'Aristoxenos',8), (27550,'Schopenhauer',6), (28106,'Carnap',3), (29120,'Theophrastos',2), (29555,'Feuerbach',2);
 insert into vorlesungen(vorlnr, titel, sws, gelesenvon) values (5001,'Grundzuege',4,2137), (5041,'Ethik',4,2125), (5043,'Erkenntnistheorie',3,2126), (5049, 'Maeeutik',2,2125), (4052,'Logik',4,2125), (5052,'Wissenschaftstheorie',3,2126), (5216,'Bioethik',2,2126), (5259,'Der Wiener Kreis',2,2133), (5022,'Glaube und Wissen',2,2134), (4630,'Die 3 Kritiken',4,2137);
--- tools/generate/uni.sh
+++ tools/generate/uni.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-echo "$PWD"
-pushd ../..
-rm -r resources/data/uni
+rm -rf resources/data/uni
 mkdir -p resources/data/uni
 "build/lingodb-debug/sql" "resources/data/uni" < resources/sql/uni/initialize.sql
 
