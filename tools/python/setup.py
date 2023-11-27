edc9861bf43bfbe43b4524aa4b5a6fcfe118cf99
Michael Jungmair <jungmair@in.tum.de>
2023-11-27 14:45:50 +0100

    Set correct dependencies of lingodb package

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -3,9 +3,5 @@ from setuptools import setup
 setup(name='lingodb',
       description='',
       version='0.0.1',
-      packages=['lingodb'],
-      install_requires=[
-            'lingodb-bridge',
-      ],
-
+      packages=['lingodb']
 )
