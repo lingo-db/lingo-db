8e0b2008738b62a3d3498a22b849f9de0cdf95b5
Michael Jungmair <jungmair@in.tum.de>
2025-06-05 14:19:29 +0200

    Update ssb generation scripts to use updated ssb repository

--- tools/generate/ssb_simplified.sh
+++ tools/generate/ssb_simplified.sh
@@ -6,17 +6,8 @@ cp  resources/sql/ssb_simplified/initialize.sql $TMPDIR/initialize.sql
 cp  tools/scripts/ssb_convert_to_simplified.py $TMPDIR/ssb_convert_to_simplified.py
 
 pushd $TMPDIR
-echo 'd37618c646a6918be8ccc4bc79704061  dbgen.zip' | md5sum --check --status 2>/dev/null || curl -OL https://db.in.tum.de/~fent/dbgen/ssb/dbgen.zip
-echo 'd37618c646a6918be8ccc4bc79704061  dbgen.zip' | md5sum --check --status
-unzip -u dbgen.zip
-mv dbgen/* .
-rm -rf dbgen
-rm dbgen.zip
-rm -rf ./*.tbl
-sed -i 's/#define  MAXAGG_LEN    10/#define  MAXAGG_LEN    20/' shared.h
-sed -i 's/O_CREAT)/O_CREAT,0644)/' bm_utils.c
-
-make dbgen
+git clone https://github.com/lingo-db/ssb-dbgen.git
+cmake -B . ssb-dbgen && cmake --build .
 SF=$2
 ./dbgen -f -T c -s "$SF"
 ./dbgen -qf -T d -s "$SF"
