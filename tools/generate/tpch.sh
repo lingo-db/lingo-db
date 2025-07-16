10d04702a47020d93f80ec66dda00be0b4851d84
Abdelrahman Orief <git@aorief.com>
2025-07-16 14:10:14 +0200

    housekeeping

--- tools/generate/tpch.sh
+++ tools/generate/tpch.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 TMPDIR=`mktemp --directory`
 echo $TMPDIR
-cp  resources/sql/tpch/initialize.sql $TMPDIR/initialize.sql
 pushd $TMPDIR
 wget -q https://github.com/electrum/tpch-dbgen/archive/32f1c1b92d1664dba542e927d23d86ffa57aa253.zip -O tpch-dbgen.zip
 unzip -q tpch-dbgen.zip
