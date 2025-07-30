70f414f9803366f9116a963779da20a1c1f98667
Lukas Döllerer <contact@lukas-doellerer.de>
2025-07-30 15:18:11 +0000

    split impl

---
+++ src/execution/baseline/CompilerConfig.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <tpde/x64/CompilerX64.hpp>
+
+namespace lingodb::execution::baseline {
+    // we will use the default config
+    // NOLINTBEGIN(readability-identifier-naming)
+    struct CompilerConfig : tpde::x64::PlatformConfig {
+    };
+
+    // NOLINTEND(readability-identifier-naming)
+}
