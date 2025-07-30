4d46a41441f7f6bb4a92e96b25d34316b9b9649b
Moritz Bachmaier <msb@bachmaiers.de>
2025-07-30 10:25:47 +0200

    parser, analyzer, translator: add first support for grouping sets (rollup) and grouping functions; fixed some smaller bugs (wip)

--- include/lingodb/compiler/frontend/ast/bound/bound_groupby.h
+++ include/lingodb/compiler/frontend/ast/bound/bound_groupby.h
@@ -6,11 +6,24 @@
 namespace lingodb::ast {
 class BoundGroupByNode : public AstNode {
    public:
-   BoundGroupByNode(std::vector<std::shared_ptr<NamedResult>> groupNamedResults);
+   BoundGroupByNode(std::vector<std::shared_ptr<NamedResult>> groupNamedResults, std::vector<std::set<size_t>> groupingSet);
    //! The total set of all group expressions
    std::vector<std::shared_ptr<NamedResult>> groupNamedResults;
 
-   //TODO having clause
+   std::vector<std::vector<std::shared_ptr<NamedResult>>>   localGroupByNamedResults, localMapToNullNamedResults,
+                                                            localNotAvailableNamedResults, localAggregationNamedResults;
+
+   std::vector<std::pair<size_t, std::shared_ptr<NamedResult>>>  localPresentIntval;
+
+
+   std::vector<std::vector<std::shared_ptr<NamedResult>>> unionNamedResults;
+
+   std::vector<std::pair<size_t, std::shared_ptr<NamedResult>>>  groupingFunctions;
+
+   std::vector<std::set<size_t>> groupingSet;
+
+
+
 
    std::string toDotGraph(uint32_t depth, NodeIdGenerator& idGen) override;
 };
--- src/compiler/frontend/ast/bound/bound_groupby.cpp
+++ src/compiler/frontend/ast/bound/bound_groupby.cpp
@@ -1,6 +1,6 @@
 #include "lingodb/compiler/frontend/ast/bound/bound_groupby.h"
 namespace lingodb::ast {
-BoundGroupByNode::BoundGroupByNode(std::vector<std::shared_ptr<NamedResult>> groupNamedResults) : AstNode(NodeType::BOUND_GROUP_BY), groupNamedResults(groupNamedResults) {
+BoundGroupByNode::BoundGroupByNode(std::vector<std::shared_ptr<NamedResult>> groupNamedResults, std::vector<std::set<size_t>> groupingSet) : AstNode(NodeType::BOUND_GROUP_BY), groupNamedResults(groupNamedResults), groupingSet(groupingSet) {
 }
 std::string BoundGroupByNode::toDotGraph(uint32_t depth, NodeIdGenerator& idGen) {
    return "";
