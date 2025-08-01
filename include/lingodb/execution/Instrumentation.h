f33bc5bb2192bff07829c6f9c832c3f2b33d7edb
Michael Jungmair <jungmair@in.tum.de>
2025-08-01 14:43:32 +0200

    remove unnecessary MLIR includes in runtime/catalog/execution

--- include/lingodb/execution/Instrumentation.h
+++ include/lingodb/execution/Instrumentation.h
@@ -1,12 +1,13 @@
 #ifndef LINGODB_EXECUTION_INSTRUMENTATION_H
 #define LINGODB_EXECUTION_INSTRUMENTATION_H
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/Pass/PassManager.h"
 #include <memory>
 #include <string>
 #include <vector>
+
 namespace mlir {
 class PassInstrumentation;
+class PassManager;
+class ModuleOp;
 } // end namespace mlir
 namespace lingodb::execution {
 struct PassData {
@@ -15,13 +16,18 @@ struct PassData {
    std::string description;
    std::string argument;
 };
+
 struct SnapshotState {
    bool serialize = false;
    size_t counter = 0;
    std::vector<PassData> passes;
 };
+
 std::string getSnapshotDir();
+
 void addLingoDBInstrumentation(mlir::PassManager& pm, std::shared_ptr<SnapshotState> serializationState);
-void snapshotImportantStep(std::string shortName, mlir::ModuleOp& moduleOp, std::shared_ptr<SnapshotState> serializationState);
+
+void snapshotImportantStep(std::string shortName, mlir::ModuleOp& moduleOp,
+                           std::shared_ptr<SnapshotState> serializationState);
 } // end namespace lingodb::execution
 #endif //LINGODB_EXECUTION_INSTRUMENTATION_H
