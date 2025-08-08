ff9eb7837c71f790ae7b8cafc0f74ea34dd8df6c
Lukas Döllerer <lukas.doellerer@tum.de>
2025-08-08 11:49:18 +0200

    add csv print option to sql.cpp

--- src/tools/sql.cpp
+++ src/tools/sql.cpp
@@ -7,15 +7,20 @@
 
 #include "lingodb/compiler/mlir-support/eval.h"
 #include "lingodb/execution/Execution.h"
+#include "lingodb/utility/Setting.h"
 
 namespace {
 using namespace lingodb;
-class ConciseTimingPrinter : public execution::TimingProcessor {
+utility::GlobalSetting<bool> reportTimes("sql.report_times", false);
+utility::GlobalSetting<std::string> reportFormat("sql.report_format", "simple");
+utility::GlobalSetting<bool> prompt("sql.prompt", true);
+
+class ConciseConsoleTimingPrinter : public execution::TimingProcessor {
    double compilation;
    double execution;
 
    public:
-   ConciseTimingPrinter() : compilation(0.0), execution(0.0) {
+   ConciseConsoleTimingPrinter() : compilation(0.0), execution(0.0) {
    }
    void addTiming(const std::unordered_map<std::string, double>& timing) override {
       for (auto [name, t] : timing) {
@@ -30,10 +35,68 @@ class ConciseTimingPrinter : public execution::TimingProcessor {
       std::cerr << " compilation: " << compilation << " [ms] execution: " << execution << " [ms]" << std::endl;
    }
 };
-void handleQuery(runtime::Session& session, std::string sqlQuery, bool reportTimes) {
+
+class LoggingTimingPrinter : public execution::TimingProcessor {
+   std::unordered_map<std::string, double> timings;
+
+   public:
+   LoggingTimingPrinter() = default;
+
+   void addTiming(const std::unordered_map<std::string, double>& timing) override {
+      for (auto [name, t] : timing) {
+         if (name == "executionTime") {
+            timings["execution"] += t;
+         } else if (name == "QOpt") {
+            timings["qopt"] += t;
+         } else if (name == "lowerRelAlg") {
+            timings["lowerrelalg"] += t;
+         } else if (name == "lowerSubOp") {
+            timings["lowersubop"] += t;
+         } else if (name == "lowerDB") {
+            timings["lowerdb"] += t;
+         } else if (name == "lowerArrow") {
+            timings["lowerarrow"] += t;
+         } else if (name == "lowerToLLVM" || name == "baselineLowering" || name == "toLLVMIR") {
+            timings["lowerforbackend"] += t;
+         } else if (name == "llvmOptimize") {
+            timings["backendoptimize"] += t;
+         } else if (name == "llvmCodeGen" || name == "baselineCodeGen") {
+            timings["backendcodegen"] += t;
+         } else if (name == "baselineEmit") {
+            timings["backendlink"] += t;
+         } else {
+            std::cerr << "Unknown timing entry: " << name << " with value: " << t << std::endl;
+            abort();
+         }
+      }
+   }
+
+   void process() override {
+      // Output CSV header
+      for (auto it = timings.begin(); it != timings.end(); ++it) {
+         std::cerr << it->first;
+         if (std::next(it) != timings.end()) std::cerr << ",";
+      }
+      std::cerr << std::endl;
+      // Output CSV values
+      for (auto it = timings.begin(); it != timings.end(); ++it) {
+         std::cerr << it->second;
+         if (std::next(it) != timings.end()) std::cerr << ",";
+      }
+      std::cerr << std::endl;
+   }
+};
+void handleQuery(runtime::Session& session, std::string sqlQuery) {
    auto queryExecutionConfig = execution::createQueryExecutionConfig(execution::getExecutionMode(), true);
-   if (reportTimes) {
-      queryExecutionConfig->timingProcessor = std::make_unique<ConciseTimingPrinter>();
+   if (reportTimes.getValue()) {
+      if (reportFormat.getValue() == "simple") {
+         queryExecutionConfig->timingProcessor = std::make_unique<ConciseConsoleTimingPrinter>();
+      } else if (reportFormat.getValue() == "csv") {
+         queryExecutionConfig->timingProcessor = std::make_unique<LoggingTimingPrinter>();
+      } else {
+         std::cerr << "Unknown report format: " << reportFormat.getValue() << std::endl;
+         return;
+      }
    }
    auto executer = execution::QueryExecuter::createDefaultExecuter(std::move(queryExecutionConfig), session);
    executer->fromData(sqlQuery);
@@ -50,27 +113,16 @@ int main(int argc, char** argv) {
       std::cerr << "USAGE: sql database" << std::endl;
       return 1;
    }
-
-   bool reportTimes = false;
-   if (const char* reportTimesEnv = std::getenv("LINGODB_SQL_REPORT_TIMES")) {
-      reportTimes = std::stoll(reportTimesEnv);
-   }
-   bool prompt = true;
-   if (const char* promptEnv = std::getenv("LINGODB_SQL_PROMPT")) {
-      prompt = std::stoll(promptEnv);
-   }
-
    auto session = runtime::Session::createSession(std::string(argv[1]), true);
 
-   lingodb::compiler::support::eval::init();
+   compiler::support::eval::init();
    auto scheduler = scheduler::startScheduler();
 
    linenoiseSetMultiLine(true); // enables multi-line editing
-   std::string line;
    std::stringstream query;
 
    while (true) {
-      const char* promptStr = prompt ? (query.str().empty() ? "sql> " : "   -> ") : "";
+      const char* promptStr = prompt.getValue() ? (query.str().empty() ? "sql> " : "   -> ") : "";
       char* input = linenoise(promptStr);
 
       if (input == nullptr) {
@@ -79,7 +131,7 @@ int main(int argc, char** argv) {
          break;
       }
 
-      line = input;
+      std::string line = input;
       free(input); // linenoise returns malloc'd memory
 
       if (line == "exit") {
@@ -96,7 +148,7 @@ int main(int argc, char** argv) {
          // Done reading the full statement
          linenoiseHistoryAdd(query.str().c_str()); // optional: add to history
 
-         handleQuery(*session, query.str(), reportTimes);
+         handleQuery(*session, query.str());
 
          // Clear for next command
          query.str("");
