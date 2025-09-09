590fef96cb723625aa8c09a740ec9760a46bfef9
Moritz Bachmaier <msb@bachmaiers.de>
2025-09-09 09:59:35 +0200

    parser, frontend: added syntax_error for better error handling

--- include/lingodb/compiler/frontend/frontend_error.h
+++ include/lingodb/compiler/frontend/frontend_error.h
@@ -7,10 +7,19 @@ class frontend_error : public std::runtime_error {
    public:
    frontend_error();
    explicit frontend_error(const std::string& message);
-   frontend_error(const std::string& message, location& loc);
+   frontend_error(const std::string& message, const location& loc);
    const char* what() const noexcept override;
    private:
    location loc;
    std::string errorMsg;
+};
+
+class syntax_error : public frontend_error {
+   public:
+   syntax_error();
+   explicit syntax_error(const std::string& message);
+   syntax_error(const std::string& message, const location& loc);
+
+
 };
 } // namespace lingodb
