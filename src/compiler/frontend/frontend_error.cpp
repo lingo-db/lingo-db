a50ccfb8c57d36f0c3600e2151e3bf8a7a4c4bc2
Moritz Bachmaier <msb@bachmaiers.de>
2025-09-09 09:59:35 +0200

    parser, frontend: added syntax_error for better error handling

--- src/compiler/frontend/frontend_error.cpp
+++ src/compiler/frontend/frontend_error.cpp
@@ -5,7 +5,7 @@ frontend_error::frontend_error() : std::runtime_error("FrontendError") {
 }
 frontend_error::frontend_error(const std::string& message) : std::runtime_error(message) {
 }
-frontend_error::frontend_error(const std::string& message, location& loc) : std::runtime_error(message), loc(loc) {
+frontend_error::frontend_error(const std::string& message, const location& loc) : std::runtime_error(message), loc(loc) {
    std::ostringstream s{};
    s << runtime_error::what() << " at " << loc;
    errorMsg = s.str();
@@ -13,5 +13,12 @@ frontend_error::frontend_error(const std::string& message, location& loc) : std:
 }
 const char* frontend_error::what() const noexcept {
    return errorMsg.c_str();
+}
+
+syntax_error::syntax_error() {
+}
+syntax_error::syntax_error(const std::string& message) : frontend_error(message) {
+}
+syntax_error::syntax_error(const std::string& message, const location& loc) : frontend_error(message, loc) {
 }
 } // namespace lingodb
