354b4272605a6a1087794c88fd85885f39729902
Michael Jungmair <jungmair@in.tum.de>
2025-10-09 14:33:23 +0200

    Reduce latency of passes run before lowering db dialect

--- include/lingodb/compiler/Dialect/DB/Passes.h
+++ include/lingodb/compiler/Dialect/DB/Passes.h
@@ -4,8 +4,8 @@
 #include "mlir/Pass/Pass.h"
 
 namespace lingodb::compiler::dialect::db {
-std::unique_ptr<mlir::Pass> createEliminateNullsPass();
+std::unique_ptr<mlir::Pass> createPrepareLoweringPass();
 std::unique_ptr<mlir::Pass> createSimplifyToArithPass();
-std::unique_ptr<mlir::Pass> createOptimizeRuntimeFunctionsPass();
+void addOptimizeRuntimeFunctionPatterns(mlir::RewritePatternSet& patterns);
 } // end namespace lingodb::compiler::dialect::db
 #endif //LINGODB_COMPILER_DIALECT_DB_PASSES_H
