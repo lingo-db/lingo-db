42e2a9d133ed1fe18b171cd9e50f3df4341aa09f
Michael Jungmair <jungmair@in.tum.de>
2025-10-11 18:04:49 +0200

    readd cheap mode

--- include/lingodb/execution/LLVMBackends.h
+++ include/lingodb/execution/LLVMBackends.h
@@ -2,7 +2,7 @@
 #define LINGODB_EXECUTION_LLVMBACKENDS_H
 #include "Backend.h"
 namespace lingodb::execution {
-std::unique_ptr<ExecutionBackend> createDefaultLLVMBackend();
+std::unique_ptr<ExecutionBackend> createDefaultLLVMBackend(bool optimize = true);
 std::unique_ptr<ExecutionBackend> createLLVMDebugBackend();
 std::unique_ptr<ExecutionBackend> createLLVMProfilingBackend();
 std::unique_ptr<ExecutionBackend> createGPULLVMBackend();
