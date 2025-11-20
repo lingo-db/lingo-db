fa223e80b7185c4113689e4500c46a1c4295e800
Jonas Ladner <jonas.ladner+git@tum.de>
2025-11-20 21:23:29 +0100

    wip: translate the x86-specific parts of the tpde code to arm

--- src/execution/baseline/CompilerConfig.hpp
+++ src/execution/baseline/CompilerConfig.hpp
@@ -1,13 +1,23 @@
 #pragma once
 
+#if defined(__x86_64__) && false
 #include <tpde/x64/CompilerX64.hpp>
+#elif defined(__aarch64__) || true
+#include <tpde/arm64/CompilerA64.hpp>
+#endif
 
 namespace lingodb::execution::baseline {
 // NOLINTBEGIN(readability-identifier-naming)
 
 // use the default config
+#if defined(__x86_64__) && false
 struct CompilerConfig : tpde::x64::PlatformConfig {
 };
+#elif defined(__aarch64__) || true
+struct CompilerConfig : tpde::a64::PlatformConfig {
+};
+#endif
+
 
 // NOLINTEND(readability-identifier-naming)
 }
