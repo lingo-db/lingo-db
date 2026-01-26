23e175342a131b273f56a64d7206aad01257d946
Moritz Bachmaier <msb@bachmaiers.de>
2026-01-26 12:44:36 +0100

    fixed sip for subquery inside select clause; fixed issue for multiple equal hash keys

--- src/compiler/Dialect/SubOperator/Transforms/SIPPass.cpp
+++ src/compiler/Dialect/SubOperator/Transforms/SIPPass.cpp
@@ -54,7 +54,6 @@ class SIPPass : public mlir::PassWrapper<SIPPass, mlir::OperationPass<mlir::Modu
       llvm::SmallVector<std::string> buildKeyColumnNames; // Columns used for hash key on build side
       llvm::SmallVector<std::string> probeKeyColumnsNames; // Columns used for hash key on probe side
       subop::GetExternalOp externalProbeOp;
-      bool isPositiveJoin; // True for IN/all_true joins, false for NOT IN/none_true joins
    };
 
    std::pair<mlir::Operation*, std::vector<subop::Member>> walkToFindSourceScan(mlir::Operation* op, std::vector<tuples::ColumnRefAttr> keys, bool debug = false) {
@@ -152,11 +151,21 @@ class SIPPass : public mlir::PassWrapper<SIPPass, mlir::OperationPass<mlir::Modu
    //Finds source scan for the build side
    std::pair<mlir::Operation*, std::vector<subop::Member>> findSourceScanFromCreateHashIndexedView(subop::CreateHashIndexedView createHashIndexedView, bool debug) {
       auto users = createHashIndexedView.getOperand().getUsers();
+      bool isValid = true;
+      subop::MapOp m;
       for (auto* user : users) {
          if (auto mat = mlir::dyn_cast_or_null<subop::MaterializeOp>(user)) {
             mlir::Value streamBeforeMat = mat->getOperand(0);
             std::vector<tuples::ColumnRefAttr> buildKeyColumns;
             if (auto mapOp = mlir::dyn_cast_or_null<subop::MapOp>(streamBeforeMat.getDefiningOp())) {
+               mapOp.getRegion().walk([&](db::Hash hashOp) {
+                  if (hashOp.getVal().size() > 1) {
+                     isValid = false;
+                     m = mapOp;
+                  }
+               });
+
+
                // Extract the input columns attribute from the map operation
                auto inputAttr = mapOp.getInputColsAttr();
                for (auto input : inputAttr) {
@@ -178,6 +187,8 @@ class SIPPass : public mlir::PassWrapper<SIPPass, mlir::OperationPass<mlir::Modu
             }
             if (buildKeyColumns.size() > 1) {
                std::cerr << "Unsupported key size: " << buildKeyColumns.size() << std::endl;
+            } else if (!isValid) {
+               return {nullptr, {}};
             }
             return walkToFindSourceScan(mat, buildKeyColumns, debug);
          }
@@ -223,6 +234,7 @@ class SIPPass : public mlir::PassWrapper<SIPPass, mlir::OperationPass<mlir::Modu
       auto* stateDefOp = stateValue.getDefiningOp();
       auto createHashView = mlir::dyn_cast_or_null<subop::CreateHashIndexedView>(stateDefOp);
       if (!createHashView) return std::nullopt;
+
       //First get Build/Input side
       auto [buildScan, buildKeyMembers] = findSourceScanFromCreateHashIndexedView(createHashView, debug);
       if (debug) {
@@ -275,28 +287,59 @@ class SIPPass : public mlir::PassWrapper<SIPPass, mlir::OperationPass<mlir::Modu
       // Detect if this is a positive (IN/all_true) or negative (NOT IN/none_true) join
       //TODO detection may be hardly simplified
       //TODO einmal selber machen
-      bool isPositiveJoin = true;
+      bool isAntiJoin = false;
+      bool hasMarkerState = false;
+      bool markerIsFiltered = false;
+
       for (auto* user : lookupOp->getUsers()) {
          if (auto nestedMap = mlir::dyn_cast_or_null<subop::NestedMapOp>(user)) {
+            //Check if is anti join
             nestedMap.walk([&](subop::FilterOp filter) {
                if (filter.getFilterSemantic() == subop::FilterSemantic::none_true) {
-                  isPositiveJoin = false;
+                  isAntiJoin = true;
                }
             });
+
+            // Check if markers are created
+            nestedMap.walk([&](subop::CreateSimpleStateOp createState) {
+               hasMarkerState = true;
+            });
+
+            // Walk the nested_map to find if the marker is filtered after the scan, if not we cannot use SIP
+            if (hasMarkerState) {
+               nestedMap.walk([&](subop::ScanOp scanOp) {
+                  if (auto stateType = mlir::dyn_cast_or_null<subop::SimpleStateType>(scanOp.getState().getType())) {
+                     for (auto* scanUser : scanOp->getUsers()) {
+                        if (auto filterOp = mlir::dyn_cast_or_null<subop::FilterOp>(scanUser)) {
+                           markerIsFiltered = true;
+                           break;
+                        }
+                     }
+                  }
+               });
+            }
          }
       }
-      if (!isPositiveJoin) {
+      //AntiJoins cannot use current SIP implementions
+      if (isAntiJoin) {
+         return std::nullopt;
+      }
+      // Correlated subqueries (with outer references in predicates) cannot use SIP
+      if (hasMarkerState && !markerIsFiltered) {
          return std::nullopt;
       }
 
-      return HashJoinInfo{.buildSideScan = mlir::dyn_cast_or_null<subop::ScanOp>(buildScan),
-                          .probeSideRoot = probeScan->getResult(0),
-                          .hashView = createHashView,
-                          .lookupOp = lookupOp,
-                          .buildKeyColumnNames = buildKeyNames,
-                          .probeKeyColumnsNames = probeKeyColumnsNames,
-                          .externalProbeOp = externalOp,
-                          .isPositiveJoin = isPositiveJoin};
+      // For mark joins (EXISTS), only apply SIP if the marker is actually used for filtering
+      // For regular joins, always apply SIP
+      return HashJoinInfo{
+         .buildSideScan = mlir::dyn_cast_or_null<subop::ScanOp>(buildScan),
+         .probeSideRoot = probeScan->getResult(0),
+         .hashView = createHashView,
+         .lookupOp = lookupOp,
+         .buildKeyColumnNames = buildKeyNames,
+         .probeKeyColumnsNames = probeKeyColumnsNames,
+         .externalProbeOp = externalOp,
+      };
    }
    std::string gen_random(const int len) {
       static const char alphanum[] =
@@ -370,9 +413,6 @@ class SIPPass : public mlir::PassWrapper<SIPPass, mlir::OperationPass<mlir::Modu
                   for (auto& in : joinInfo->buildKeyColumnNames) {
                      std::cerr << " - " << in;
                   }
-
-
-
 
                   std::cerr << "\n\nProbeSide: \n";
                   for (auto& in : joinInfo->probeKeyColumnsNames) {
