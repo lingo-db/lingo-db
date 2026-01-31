ad96aef9dc99ea82bf3c290841cb0a85fd4e8952
Michael Jungmair <jungmair@in.tum.de>
2026-01-31 18:40:13 +0100

    Reduce overhead of generating runtime function calls

--- include/lingodb/compiler/Dialect/util/FunctionHelper.h
+++ include/lingodb/compiler/Dialect/util/FunctionHelper.h
@@ -7,6 +7,19 @@ template <class T>
 static void* getPtr(T x) {
    return *reinterpret_cast<void**>(&x);
 }
+class FunctionSpec;
+class FunctionHelper {
+   mlir::ModuleOp parentModule;
+   std::unordered_map<std::string, std::pair<mlir::func::FuncOp,mlir::SymbolRefAttr>>  loadedFunctions;
+
+   public:
+   static void visitAllFunctions(const std::function<void(std::string, void*)>& fn);
+   void setParentModule(const mlir::ModuleOp& parentModule);
+
+   public:
+   static mlir::func::CallOp call(mlir::OpBuilder& builder, mlir::Location loc, const FunctionSpec& function, mlir::ValueRange values);
+   static mlir::Value convertValue(mlir::OpBuilder& builder, mlir::Value v, mlir::Type t, mlir::Location loc);
+};
 class FunctionSpec {
    std::string name;
    std::string mangledName;
@@ -31,20 +44,12 @@ class FunctionSpec {
 
    FunctionSpec(const std::string& name, const std::string& mangledName, const std::function<std::vector<mlir::Type>(mlir::MLIRContext*)>& parameterTypes, const std::function<std::vector<mlir::Type>(mlir::MLIRContext*)>& resultTypes, void* (*getPointer)());
 
-   std::function<mlir::ResultRange(mlir::ValueRange)> operator()(mlir::OpBuilder& builder, mlir::Location loc) const;
+   auto  operator()(mlir::OpBuilder& builder, mlir::Location loc) const {
+      return [&builder, loc, this](mlir::ValueRange range) -> mlir::ResultRange { return lingodb::compiler::dialect::util::FunctionHelper::call(builder, loc, *this, range).getResults(); };
+   }
 };
 
-class FunctionHelper {
-   mlir::ModuleOp parentModule;
 
-   public:
-   static void visitAllFunctions(const std::function<void(std::string, void*)>& fn);
-   void setParentModule(const mlir::ModuleOp& parentModule);
-
-   public:
-   static mlir::func::CallOp call(mlir::OpBuilder& builder, mlir::Location loc, const FunctionSpec& function, mlir::ValueRange values);
-   static mlir::Value convertValue(mlir::OpBuilder& builder, mlir::Value v, mlir::Type t, mlir::Location loc);
-};
 } // namespace lingodb::compiler::dialect::util
 
 #endif //LINGODB_COMPILER_DIALECT_UTIL_FUNCTIONHELPER_H
