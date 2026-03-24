97c1d7abe9815cb30439776a139841c269cae17c
Jonas Ladner <jonas.ladner+git@tum.de>
2026-03-24 14:32:23 +0100

    [no ci] wip (non-working): use loop operation instead of unrolling, fix materialize using newly created placeholder values instead of existing values

--- src/compiler/Conversion/RelAlgToSubOp/RelAlgToSubOp.cpp
+++ src/compiler/Conversion/RelAlgToSubOp/RelAlgToSubOp.cpp
@@ -3031,7 +3031,7 @@ class QueryReturnOpLowering : public OpConversionPattern<relalg::QueryReturnOp> 
    }
 };
 
-class LowerLocalTableScan : public OpConversionPattern<relalg::LocalTableScanOp> {
+class LocalTableScanLowering : public OpConversionPattern<relalg::LocalTableScanOp> {
    public:
    using OpConversionPattern::OpConversionPattern;
 
@@ -3127,7 +3127,7 @@ void RelalgToSubOpLoweringPass::runOnOperation() {
    patterns.insert<TrackTuplesLowering>(ctxt);
    patterns.insert<QueryOpLowering>(ctxt);
    patterns.insert<QueryReturnOpLowering>(ctxt);
-   patterns.insert<LowerLocalTableScan>(ctxt);
+   patterns.insert<LocalTableScanLowering>(ctxt);
 
    if (failed(applyFullConversion(module, target, std::move(patterns))))
       signalPassFailure();
